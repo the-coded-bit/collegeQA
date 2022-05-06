b7facdc988435916456fc548b2be78f8ef244175
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-05-06 05:56:28 +0530

    feat[contexts]: add contexts to preserve user logging status

---
+++ contexts/authWrapper.js
@@ -0,0 +1,71 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { auth } from '../firebase/firebase';
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
+
+export const authContext = createContext({ authUser: null, loading: true });
+
+function AuthWrapper({ children }) {
+
+    //user and fetching user from firebase i.e. loading status states
+    const [authUser, setAuthUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    //default use Effect
+    useEffect(() => {
+        console.log('use effect of authwrapper');
+        onAuthStateChanged(auth, async (user) => {
+            console.log('on auth state changed triggered');
+            if (!user) {
+                console.log('user not found');
+                setAuthUser(null);
+                setLoading(false);
+            }
+            else {
+                setLoading(true);
+                console.log('printing user from use effect of authwrapper =', user);
+                setAuthUser(user);
+                setLoading(false);
+            }
+        });
+    }, []);
+
+
+    //authOptions
+
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+
+    function logout() {
+        return signOut(auth);
+    }
+
+
+    //creating authOptions with functionalities of signin, signout, login
+    const authOptions = {
+        signup,
+        login,
+        logout,
+        setAuthUser,
+        authUser,
+        loading,
+        setLoading
+    }
+
+    console.log('render of authWrapper');
+    return (
+        <authContext.Provider value={authOptions}>
+            {!loading && children}
+        </authContext.Provider>
+    )
+}
+
+export default AuthWrapper
