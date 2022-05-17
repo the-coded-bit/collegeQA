ea68fe32a335b324b0c233823c48e95c4f704af0
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-05-17 09:44:54 +0530

    add profile Image, last signIn, questions from firebase

--- pages/profile.js
+++ pages/profile.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Page from '../layouts/Page'
 import Card from '../layouts/Card';
+import { authContext } from '../contexts/authWrapper';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 
 function profile() {
 
-  const questions = [
-    'what is the meaning of my name?',
-    'what am i to do in my 20s',
-    'which college mess is good?',
-    'feeling drowsy all day what to do?'
-  ]
+  // grabbing user 
+  const { authUser } = useContext(authContext);
+
+  //states for Profile Image and question and answer
+  const [profileImage, setprofileImage] = useState('');
+  const [questions, setQuestions] = useState([]);
+  const [memberData, setMemberData] = useState('loading...');
+
+  useEffect(() => {
+    const initSetup = async () => {
+      const docSnap = await getDoc(doc(db, `users/${authUser.uid}`));
+
+      const allQuestionPromises = docSnap.data().questionsAsked.map(async (val) => {
+        const snap = await getDoc(doc(db, `questions/${val}`));
+        return snap.data().question;
+      });
+
+      const questionPromiseResolved = await Promise.all(allQuestionPromises);
+      console.log(questionPromiseResolved);
+
+      // resolving the account creation time
+      const timestamp = parseInt(authUser.metadata.createdAt);
+      const d = new Date(timestamp);
+
+      setprofileImage(docSnap.data().photoURL);
+      setQuestions(questionPromiseResolved);
+      setMemberData(d.toDateString());
+    }
+
+    initSetup();
+
+  }, []);
 
   const answers = [
     'to give is the meaning of the your name(donate)',
@@ -21,7 +50,7 @@ function profile() {
   return (
     <Page bgColor='bg-slate-100' title='Profile' content='Profile Page of User'>
       <Card classitems='flex justify-center flex-col mb-2 bg-white items-center'>
-        <div className='bg-red-700 w-44 h-44 rounded-full'></div>
+        <img className='bg-red-700 w-44 h-44 rounded-full' src={profileImage}></img>
         <div className='text-3xl font-bold'>Name</div>
         <Card classitems='bg-slate-100 m-4 p-2 w-11/12 shadow-md'>
           <div className='flex justify-between items-center mb-2 border-b-2 border-slate-400 pb-2'>
@@ -30,7 +59,7 @@ function profile() {
           </div>
           <div className='flex justify-between items-center '>
             <span>Member Since</span>
-            <span className='w-24'>Nov 07, 2016</span>
+            <span className='w-26'>{memberData}</span>
           </div>
         </Card>
       </Card>
