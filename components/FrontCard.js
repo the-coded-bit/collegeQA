e041e30f3e2b90d5e3022efd701bfe0f46d24264
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-05-21 06:27:06 +0530

    feat : add View More, when clicking redirect to answer route

--- components/FrontCard.js
+++ components/FrontCard.js
@@ -2,10 +2,14 @@ import { doc, getDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase/firebase';
 import Card from '../layouts/Card';
+import {useRouter} from 'next/router';
 
 function FrontCard({ questionId, stringValue, setAnswerModal, setquestionID }) {
 
     const imageUrl = 'https://source.unsplash.com/1600x900/?beach';
+
+    // grabbing router
+    const router = useRouter();
 
     // state for name, date, answer
     const [name, setName] = useState('');
@@ -60,12 +64,12 @@ function FrontCard({ questionId, stringValue, setAnswerModal, setquestionID }) {
                         <div>{date}</div>
                     </div>
                 </div>
-                <button className='mx-2 p-2 border-2 border-indigo-600 rounded-full'>View more</button>
+                <button className='mx-2 p-2 border-2 border-indigo-600 rounded-full' onClick = {() => router.push(`/answer?id=${questionId}`)}>View more</button>
             </div>
             <div className='group h-4/5 flex flex-col justify-center items-center relative'>
                 <div className='question m-2 h-1/5 font-bold group-hover:opacity-50'>{stringValue}</div>
-                <div className='answer m-2 h-3/5 overflow-hidden group-hover:opacity-50'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                <div className='answer m-2 h-3/5 overflow-hidden group-hover:opacity-50 text-left'>
+                    {answer}
                 </div>
                 <button
                     className='bg-indigo-600 rounded-xl text-sm p-2 font-medium text-white hidden group-hover:block absolute m-auto shadow-xl'
