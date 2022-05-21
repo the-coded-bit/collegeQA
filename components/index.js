6d9bd8d54d8673ac4718e4ff176aa2d491106684
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-05-21 05:56:17 +0530

    add base file to export the components

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import QuestionModal from "./QuestionModal";
+
+export {default as QuestionModal} from './QuestionModal'
+export {default as AnswerModal} from './AnswerModal'
+export {default as Navbar} from './navbar'
+export {default as FrontCard} from './FrontCard'
+export {default as Answer} from './Answer'
+export {default as Question} from './Question'
