1768c5b3fe0aa41ef18fff3bf996f6ab98117a31
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-05-21 06:04:39 +0530

    feat[view question/answer btn] : add push route onclicking view question/answer button

--- pages/profile.js
+++ pages/profile.js
@@ -4,24 +4,33 @@ import Card from '../layouts/Card';
 import { authContext } from '../contexts/authWrapper';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
+import { useRouter } from 'next/router';
 
 function profile() {
+
+  //grabbing router
+  const router = useRouter();
+
 
   // grabbing user 
   const { authUser } = useContext(authContext);
 
   //states for Profile Image and question and answer
   const [profileImage, setprofileImage] = useState('');
+  const [name, setName] = useState('Name');
   const [questions, setQuestions] = useState([]);
   const [memberData, setMemberData] = useState('loading...');
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     const initSetup = async () => {
+
+      //resolving questions asked by user
       const docSnap = await getDoc(doc(db, `users/${authUser.uid}`));
 
       const allQuestionPromises = docSnap.data().questionsAsked.map(async (val) => {
         const snap = await getDoc(doc(db, `questions/${val}`));
-        return snap.data().question;
+        return snap.data();
       });
 
       const questionPromiseResolved = await Promise.all(allQuestionPromises);
@@ -31,27 +40,29 @@ function profile() {
       const timestamp = parseInt(authUser.metadata.createdAt);
       const d = new Date(timestamp);
 
+      // TODO : resolve the question whose answers given by user
+      const allAnswers = docSnap.data().answersGiven.map((val) => {
+        const { answer = '', questionId = '' } = val || {};
+        return { answer, questionId };
+      })
+
       setprofileImage(docSnap.data().photoURL);
       setQuestions(questionPromiseResolved);
+      setName(docSnap.data().name);
       setMemberData(d.toDateString());
+      console.log(allAnswers);
+      setAnswers(allAnswers);
     }
 
     initSetup();
 
   }, []);
 
-  const answers = [
-    'to give is the meaning of the your name(donate)',
-    'focus on health, goal, networking',
-    'i dunnknow',
-    'i feel the same hahaha!!!'
-  ]
-
   return (
     <Page bgColor='bg-slate-100' title='Profile' content='Profile Page of User'>
       <Card classitems='flex justify-center flex-col mb-2 bg-white items-center'>
         <img className='bg-red-700 w-44 h-44 rounded-full' src={profileImage}></img>
-        <div className='text-3xl font-bold'>Name</div>
+        <div className='text-3xl font-bold'>{name}</div>
         <Card classitems='bg-slate-100 m-4 p-2 w-11/12 shadow-md'>
           <div className='flex justify-between items-center mb-2 border-b-2 border-slate-400 pb-2'>
             <span>Status</span>
@@ -70,8 +81,12 @@ function profile() {
           {
             questions.map((question) => (
               <div className='group flex justify-between items-center mb-2 border-b-2 border-slate-400 pb-2'>
-                <span className='truncate w-full group-hover:hidden'>{question}</span>
-                <button className=' group-hover:block bg-indigo-600 hidden font-semibold text-white text-sm p-2 rounded-md m-auto'>View Answers</button>
+                <span className='truncate w-full group-hover:hidden'>{question.question}</span>
+                <button
+                  className=' group-hover:block bg-indigo-600 hidden font-semibold text-white text-sm p-2 rounded-md m-auto'
+                  onClick={() => router.push(`/answer?id=${question.questionId}`)}>
+                  View Answers
+                </button>
               </div>
             ))
           }
@@ -83,8 +98,12 @@ function profile() {
           {
             answers.map((answer) => (
               <div className='group flex justify-between items-center mb-2 border-b-2 border-slate-400 pb-2'>
-                <span className='group-hover:hidden truncate w-full'>{answer}</span>
-                <button className='group-hover:block bg-indigo-600 hidden text-white font-semibold text-sm p-2 rounded-md m-auto'>View Question</button>
+                <span className='group-hover:hidden truncate w-full'>{answer.answer}</span>
+                <button
+                  className='group-hover:block bg-indigo-600 hidden text-white font-semibold text-sm p-2 rounded-md m-auto'
+                  onClick={() => router.push(`/question?id=${answer.questionId}`)}>
+                  View Question
+                </button>
               </div>
             ))
           }
